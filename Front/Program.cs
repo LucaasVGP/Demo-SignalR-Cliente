a9a0c26c6078b2bd5657638a99ab75ef4d28af38
Lucas V Guerra <lucaas.vgp@gmail.com>
2024-06-26 14:35:14 -0300

    feat: adiciona projeto completo

---
+++ Front/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+		   .WithUrl("https://localhost:7284/signalrhub")
+		   .Build();
+
+connection.On<string>("ReceiveMessage", (message) =>
+{
+	Console.WriteLine($"Recebido: {message}");
+});
+
+await connection.StartAsync();
+
+Console.WriteLine("Conectado ao servidor SignalR. Pressione qualquer tecla para sair...");
+Console.ReadKey();
